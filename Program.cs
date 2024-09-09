1e8a2067c7dd8eb937e4ab5f68ea97c074921702
Samuel Järvå <samuel.jarva@iths.se>
2024-09-09 15:30:22 +0200

    Add correct text highlighter, number counter

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ class Program
 
         static void FindSequenceOfNumbersAndTurnRed(string mainString)
         {
+            List<int> numbers = new List<int>();
+            Console.WriteLine();
             for (int i = 0; i < mainString.Length; i++)
             {
                 // Checks if current char is a digit, otherwise moves to the next one
@@ -19,22 +21,33 @@ class Program
                     // Sets the current number and searches for the next one as well
                     char currentNumber = mainString[i];
                     int nextIndex = mainString.IndexOf(currentNumber, i + 1);
+                    int redLength = nextIndex - i + 1;
 
                     // nextIndex becomes -1 by default if it can't find the next index
                     if (nextIndex != -1)
                     {
                         // Cuts mainString up into 3 different substrings, the ones in the middle are red
                         string firstSub = mainString.Substring(0, i);
-                        string redSub = mainString.Substring(i, nextIndex);
+                        string redSub = mainString.Substring(i, redLength);
                         string lastSub = mainString.Substring(i + redSub.Length);
 
                         // Puts all three substrings together, with ANSI escape characters in between that changes the color to red
                         string totalString = firstSub + "\u001b[31m" + redSub + "\u001b[0m" + lastSub;
 
                         Console.WriteLine(totalString);
+
+                        int currentNumbers = Int32.Parse(redSub);
+                        numbers.Add(currentNumbers);
                     }
-                }
+                }          
             }
+            int result = 0;
+            foreach (int number in numbers)
+            {
+                result += number;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"The total of all highlighted numbers are: {result}");
         }
     }
 }
