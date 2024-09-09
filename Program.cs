84f012d5649b0ca596e153a02579a5f5ad96f82b
Samuel Järvå <samuel.jarva@iths.se>
2024-09-09 14:26:30 +0200

    Add almost fully functional code

--- Program.cs
+++ Program.cs
@@ -13,18 +13,26 @@ class Program
         {
             for (int i = 0; i < mainString.Length; i++)
             {
+                // Checks if current char is a digit, otherwise moves to the next one
                 if (char.IsDigit(mainString[i]))
                 {
+                    // Sets the current number and searches for the next one as well
                     char currentNumber = mainString[i];
-                    int nextIndex = mainString.IndexOf(currentNumber, i);
+                    int nextIndex = mainString.IndexOf(currentNumber, i + 1);
 
-                    string firstSub = mainString.Substring(0, i);
-                    string redSub = mainString.Substring(currentNumber, nextIndex);
-                    string lastSub = mainString.Substring(nextIndex + redSub.Length);
+                    // nextIndex becomes -1 by default if it can't find the next index
+                    if (nextIndex != -1)
+                    {
+                        // Cuts mainString up into 3 different substrings, the ones in the middle are red
+                        string firstSub = mainString.Substring(0, i);
+                        string redSub = mainString.Substring(i, nextIndex);
+                        string lastSub = mainString.Substring(i + redSub.Length);
 
-                    string totalString = firstSub + "\u001b[31m" + redSub + "\u001b[0m" + lastSub;
+                        // Puts all three substrings together, with ANSI escape characters in between that changes the color to red
+                        string totalString = firstSub + "\u001b[31m" + redSub + "\u001b[0m" + lastSub;
 
-                    Console.WriteLine(totalString);
+                        Console.WriteLine(totalString);
+                    }
                 }
             }
         }
