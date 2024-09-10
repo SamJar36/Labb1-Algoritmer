c34214eb9457da903e2223b84ec07e239221b910
Samuel <samandreasjarva@gmail.com>
2024-09-10 21:27:01 +0200

    Add second for loop, fully functionable now

--- Program.cs
+++ Program.cs
@@ -15,29 +15,36 @@ class Program
             Console.WriteLine();
             for (int i = 0; i < mainString.Length; i++)
             {
-                // Checks if current char is a digit, otherwise moves to the next one
                 if (char.IsDigit(mainString[i]))
                 {
-                    // Sets the current number and searches for the next one as well
                     char currentNumber = mainString[i];
                     int nextIndex = mainString.IndexOf(currentNumber, i + 1);
                     int redLength = (nextIndex - i) + 1;
 
-                    // nextIndex becomes -1 by default if it can't find the next index
                     if (nextIndex != -1)
                     {
-                        // Cuts mainString up into 3 different substrings, the ones in the middle are red
-                        string firstSub = mainString.Substring(0, i);
-                        string redSub = mainString.Substring(i, redLength);
-                        string lastSub = mainString.Substring(i + redSub.Length);
+                        bool hasLetters = false;
+                        for (int j = i + 1; j < nextIndex; j++)
+                        {
+                            
+                            if (char.IsLetter(mainString[j]) || !char.IsLetterOrDigit(mainString[j]))
+                            {
+                                hasLetters = true;
+                                break;
+                            }
+                        }
+                        if (!hasLetters)
+                        {
+                            string firstSub = mainString.Substring(0, i);
+                            string redSub = mainString.Substring(i, redLength);
+                            string lastSub = mainString.Substring(i + redSub.Length);
+                            string totalString = firstSub + "\u001b[31m" + redSub + "\u001b[0m" + lastSub;
 
-                        // Puts all three substrings together, with ANSI escape characters in between that changes the color to red
-                        string totalString = firstSub + "\u001b[31m" + redSub + "\u001b[0m" + lastSub;
+                            Console.WriteLine(totalString);
 
-                        Console.WriteLine(totalString);
-
-                        ulong currentNumbers = UInt64.Parse(redSub);
-                        numbers.Add(currentNumbers);
+                            ulong parsedNumbers = UInt64.Parse(redSub);
+                            numbers.Add(parsedNumbers);
+                        }           
                     }
                 }          
             }
